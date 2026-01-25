d4b3d09548add086529eb48c7d04a83d6e2f4268
Rodrigo Dalla Dea <rodrigoddea@gmail.com>
2026-01-25 00:54:21 -0300

    Initial Commit

---
+++ floatam/floatamApp.swift
@@ -0,0 +1,17 @@
+//
+//  floatamApp.swift
+//  floatam
+//
+//  Created by Rodrigo Dalla Dea on 25/01/26.
+//
+
+import SwiftUI
+
+@main
+struct floatamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
